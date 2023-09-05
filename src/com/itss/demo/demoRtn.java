81210e7bad4b87a94014b5e61a575d42c7636148
Miguel <klausmein@hotmail.com>
2023-09-05 02:12:32 -0500

    Comienzo del Proyecto

---
+++ src/com/itss/demo/demoRtn.java
@@ -0,0 +1,13 @@
+package com.itss.demo;
+
+import com.temenos.t24.api.hook.system.RecordLifecycle;
+
+/**
+ * TODO: Document me!
+ *
+ * @author klaus
+ *
+ */
+public class demoRtn extends RecordLifecycle {
+
+}
