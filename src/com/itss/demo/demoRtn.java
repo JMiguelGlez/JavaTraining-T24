cee2af0433b5579a022b9650b7bc50b402717b27
Miguel <klausmein@hotmail.com>
2023-09-05 02:24:21 -0500

    Training Java - Day1

--- src/com/itss/demo/demoRtn.java
+++ src/com/itss/demo/demoRtn.java
@@ -1,6 +1,12 @@
 package com.itss.demo;
 
+import com.temenos.api.TField;
+import com.temenos.api.TStructure;
+import com.temenos.api.TValidationResponse;
+import com.temenos.t24.api.complex.eb.templatehook.TransactionContext;
 import com.temenos.t24.api.hook.system.RecordLifecycle;
+import com.temenos.t24.api.records.fundstransfer.FundsTransferRecord;
+
 
 /**
  * TODO: Document me!
@@ -9,5 +15,20 @@ import com.temenos.t24.api.hook.system.RecordLifecycle;
  *
  */
 public class demoRtn extends RecordLifecycle {
+    @Override
+    public TValidationResponse validateRecord(String application, String currentRecordId, TStructure currentRecord,
+            TStructure unauthorisedRecord, TStructure liveRecord, TransactionContext transactionContext) {
+        // TODO Auto-generated method stub
+        FundsTransferRecord ftRec = new FundsTransferRecord(currentRecord);
+        
+        TField debitCcy = ftRec.getDebitCurrency();
+        TField creditCcy = ftRec.getCreditCurrency();
+        
+        if(!debitCcy.getValue().equals(creditCcy.getValue())){
+            creditCcy.setError("Debit Currency and Credit Currency Diff");
+        }
+        return ftRec.getValidationResponse();
+       
+    }
 
 }
